be3ca1d32edeaeeb012c6d32c30fd62102fde01f
Roman Böhringer <r.boehringer@opencore.ch>
2023-02-17 17:15:59 +0100

    tokenURI function with line splitting

--- src/Bio.sol
+++ src/Bio.sol
@@ -26,7 +26,7 @@ contract Bio is ERC721 {
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error TokenNotMinted(uint256 tokenID);
-    error BioMustBeShorterThan200Characters(uint256 length);
+    error InvalidBioLength(uint256 length);
 
     /// @notice Initiates CSR on mainnet
     constructor() ERC721("Biography", "Bio") {
@@ -41,27 +41,68 @@ contract Bio is ERC721 {
     /// @param _id ID to query for
     function tokenURI(uint256 _id) public view override returns (string memory) {
         if (_ownerOf[_id] == address(0)) revert TokenNotMinted(_id);
-        string memory svg = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 400 200"><style>text { font-family: sans-serif; font-size: 12px; }</style>';
-        string memory text = string.concat("<text>", bio[_id], "</text>");
+        string memory bioText = bio[_id];
+        bytes memory bioTextBytes = bytes(bioText);
+        uint lengthInBytes = bioTextBytes.length;
+        // Insert a new line after 40 characters, taking into account unicode character
+        uint lines = (lengthInBytes - 1) / 40 + 1;
+        string[] memory strLines = new string[](lines);
+        bool prevByteWasContinuation;
+        uint256 insertedLines;
+        bytes memory bytesLines = new bytes(44);
+        uint bytesOffset;
+        for (uint i; i < lengthInBytes; ++i) {
+            bytes1 character = bioTextBytes[i];
+            bytesLines[bytesOffset] = character;
+            bytesOffset++;
+            if ((i > 0 && (i + 1) % 40 == 0) || prevByteWasContinuation || i == lengthInBytes - 1) {
+                bytes1 nextCharacter;
+                if (i != lengthInBytes - 1) {
+                    nextCharacter = bioTextBytes[i + 1];
+                }
+                if (nextCharacter & 0x80 == 0 || nextCharacter & 0xC0 == 0xC0 || i == lengthInBytes - 1) {
+                    // ASCII character or Unicode start byte or last byte
+                    // TODO: Technically could split on an emoji modifier
+                    assembly {
+                        mstore(bytesLines, bytesOffset)
+                    }
+                    strLines[insertedLines++] = string(bytesLines);
+                    bytesLines = new bytes(44);
+                    prevByteWasContinuation = false;
+                    bytesOffset = 0;
+                } else if (nextCharacter & 0xC0 == 0x80) {
+                    // Unicode continuation byte
+                    prevByteWasContinuation = true;
+                }
+            }
+        }
+        string
+            memory svg = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 400 100"><style>text { font-family: sans-serif; font-size: 12px; }</style>';
+        string memory text = '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">';
+        for (uint i; i < lines; ++i) {
+            text = string.concat(text, '<tspan x="50%" dy="20">', strLines[i], "</tspan>");
+        }
         string memory json = Base64.encode(
             bytes(
-                string(
-                    abi.encodePacked(
-                        '{"name": "Bio #',
-                        LibString.toString(_id),
-                        '", "image": "data:image/svg+xml;base64,',
-                        Base64.encode(bytes(string.concat(svg, text, "</svg>"))),
-                        '"}'
-                    )
+                string.concat(
+                    '{"name": "Bio #',
+                    LibString.toString(_id),
+                    '", "description": "',
+                    bioText,
+                    '", "image": "data:image/svg+xml;base64,',
+                    Base64.encode(bytes(string.concat(svg, text, "</text></svg>"))),
+                    '"}'
                 )
             )
         );
         return string(abi.encodePacked("data:application/json;base64,", json));
     }
 
+    /// @notice Mint a new Bio NFT
+    /// @param _bio The text to add
     function mint(string calldata _bio) external {
         // We check the length in bytes, so will be higher for UTF-8 characters. But sufficient for this check
-        if (bytes(_bio).length > 200) revert BioMustBeShorterThan200Characters(bytes(_bio).length);
+        if (bytes(_bio).length == 0 || bytes(_bio).length > 200) revert InvalidBioLength(bytes(_bio).length);
         uint256 tokenId = ++numMinted;
         bio[tokenId] = _bio;
         _mint(msg.sender, tokenId);
--- src/test/Bio.t.sol
+++ src/test/Bio.t.sol
@@ -5,9 +5,12 @@ import {DSTest} from "ds-test/test.sol";
 import {Utilities} from "./utils/Utilities.sol";
 import {console} from "./utils/Console.sol";
 import {Vm} from "forge-std/Vm.sol";
+import "../Bio.sol";
 
 contract ContractTest is DSTest {
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    Bio bio;
 
     Utilities internal utils;
     address payable[] internal users;
@@ -15,19 +18,15 @@ contract ContractTest is DSTest {
     function setUp() public {
         utils = new Utilities();
         users = utils.createUsers(5);
+        bio = new Bio();
     }
 
     function testExample() public {
         address payable alice = users[0];
         // labels alice's address in call traces as "Alice [<address>]"
         vm.label(alice, "Alice");
-        console.log("alice's address", alice);
-        address payable bob = users[1];
-        vm.label(bob, "Bob");
-
         vm.prank(alice);
-        (bool sent, ) = bob.call{value: 10 ether}("");
-        assertTrue(sent);
-        assertGt(bob.balance, alice.balance);
+        bio.mint(unicode"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        console.log(bio.tokenURI(1));
     }
 }
