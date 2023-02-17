b9e3cf49e99091d98c8d2b8e6c8caeacbdc303ea
Roman Böhringer <r.boehringer@opencore.ch>
2023-02-17 12:43:29 +0100

    First draft of bio protocol

---
+++ src/Bio.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {LibString} from "solmate/utils/LibString.sol";
+import {Base64} from "solady/utils/Base64.sol";
+import "../interface/Turnstile.sol";
+
+contract Bio is ERC721 {
+    /*//////////////////////////////////////////////////////////////
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Number of tokens minted
+    uint256 numMinted;
+
+    /// @notice Stores the bio value per NFT
+    mapping(uint256 => string) bio;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event BioAdded(address indexed minter, uint256 indexed nftID, string indexed bio);
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error TokenNotMinted(uint256 tokenID);
+    error BioMustBeShorterThan200Characters(uint256 length);
+
+    /// @notice Initiates CSR on mainnet
+    constructor() ERC721("Biography", "Bio") {
+        if (block.chainid == 7700) {
+            // Register CSR on Canto mainnnet
+            Turnstile turnstile = Turnstile(0xEcf044C5B4b867CFda001101c617eCd347095B44);
+            turnstile.register(tx.origin);
+        }
+    }
+
+    /// @notice Get the token URI for the specified _id
+    /// @param _id ID to query for
+    function tokenURI(uint256 _id) public view override returns (string memory) {
+        if (_ownerOf[_id] == address(0)) revert TokenNotMinted(_id);
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "Bio #',
+                        LibString.toString(_id),
+                        '", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(Utils.generateSVG(nftCharacters[_id], false))),
+                        '"}'
+                    )
+                )
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    function mint(string calldata _bio) external {
+        // We check the length in bytes, so will be higher for UTF-8 characters. But sufficient for this check
+        if (bytes(_bio).length > 200) revert BioMustBeShorterThan200Characters(bytes(_bio).length);
+        uint256 tokenId = ++numMinted;
+        bio[tokenId] = _bio;
+        _mint(msg.sender, tokenId);
+        emit BioAdded(msg.sender, tokenId, _bio);
+    }
+}
