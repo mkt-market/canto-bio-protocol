cd2e38838a03ac9440ceea127c4f80b60046496f
Roman Böhringer <r.boehringer@opencore.ch>
2023-03-23 14:26:38 +0100

    Deployment script

---
+++ script/Deploy.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >=0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/Bio.sol";
+
+contract DeploymentScript is Script {
+
+    function setUp() public {}
+
+    function run() public {
+        string memory seedPhrase = vm.readFile(".secret");
+        uint256 privateKey = vm.deriveKey(seedPhrase, 0);
+        Bio bio = new Bio();
+        vm.stopBroadcast();
+    }
+}
